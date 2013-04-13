dfc7fa7da383900bd081f29d0ae5ec77bc121baa
mason-ubuntu-12.10 <mason@ubuntu.(none)>
2013-04-12 20:45:04 -0400

    removing apt install sublime the good 'execute' command way. this only works on debian

--- cookbooks/dev-essential/recipes/default.rb
+++ cookbooks/dev-essential/recipes/default.rb
@@ -3,10 +3,9 @@ package("gitk")
 package("git-gui")
 package("vim")
 
-apt_repository "sublime-ppa" do
-  uri "http://ppa.launchpad.net/webupd8team/sublime-text-2/ubuntu"
-  distribution node['lsb']['codename']
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-end
 
+execute "add-apt-repository ppa:webupd8team/sublime-text-2"
+
+execute "apt-get update"
+
+package("sublime-text")
