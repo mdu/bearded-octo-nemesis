b5555d31dcf6927cedec9bdb1f8b24bd60dfcbec
mason-ubuntu-12.10 <mason@ubuntu.(none)>
2013-04-12 20:27:16 -0400

    trying apt adding repro

--- cookbooks/dev-essential/recipes/default.rb
+++ cookbooks/dev-essential/recipes/default.rb
@@ -2,3 +2,11 @@ package("git")
 package("gitk")
 package("git-gui")
 package("vim")
+
+apt_repository "sublime-ppa" do
+  uri "http://ppa.launchpad.net/webupd8team/sublime-text-2/ubuntu"
+  distribution node['lsb']['codename']
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+end
+
