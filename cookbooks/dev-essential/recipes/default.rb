dec1e4c5bfef3b7d3b1c811ce8f8545db252000f
mason-ubuntu-12.10 <mason@ubuntu.(none)>
2013-04-16 17:56:08 -0400

    Adding apt. install chrome and sublime

--- cookbooks/dev-essential/recipes/default.rb
+++ cookbooks/dev-essential/recipes/default.rb
@@ -3,9 +3,17 @@ package("gitk")
 package("git-gui")
 package("vim")
 
-
+# Install Sublime and Chrome
 execute "add-apt-repository ppa:webupd8team/sublime-text-2"
+
+execute "wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -"
+apt_repository "google-chrome" do
+  uri "http://dl.google.com/linux/chrome/deb/"
+  components ["stable", "main"]
+end
 
 execute "apt-get update"
 
 package("sublime-text")
+package("google-chrome-stable")
+
