7430110b8beaa382e067e839349dabf44e117487
mason-ubuntu-12.10 <mason@ubuntu.(none)>
2013-04-17 09:36:52 -0400

    Code cleanup

--- cookbooks/dev-essential/recipes/default.rb
+++ cookbooks/dev-essential/recipes/default.rb
@@ -17,32 +17,32 @@ apt_repository "google-chrome" do
   components ["stable", "main"]
 end
 
-
+# MongoDB
 apt_repository "10gen" do
   uri "http://downloads-distro.mongodb.org/repo/ubuntu-upstart"
   distribution "dist"
   components ["10gen"]
   keyserver "keyserver.ubuntu.com"
   key "7F0CEB10"
-  action :add
-  notifies :run, "execute[apt-get update]", :immediately
 end
 
+# Update repo
 execute "apt-get update"
 
 package("sublime-text")
 package("google-chrome-stable")
 package("mongodb-10gen")
+
 package("libzmq1")
 execute "ldconfig"
 
-# Install n 
+
+# Install n
 git "/tmp/n" do
   repository "https://github.com/visionmedia/n.git"
   reference "master"
   action :sync
 end
-
 execute "make install" do
 	cwd '/tmp/n'
 end
@@ -52,5 +52,3 @@ execute "n 0.8.16"
 
 # Install Redis
 package("redis-server")
-
-# Install MongoDB
