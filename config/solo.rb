ef7aa9f003a74f6653484040ab648638191c94f1
Mason Du <mdu@benbria.com>
2013-04-21 11:15:27 -0400

    retrieve home directory from the enviroment variable.

--- config/solo.rb
+++ config/solo.rb
@@ -1,3 +1,2 @@
 cookbook_path File.expand_path(File.join(File.dirname(__FILE__), '..', "cookbooks"))
 username	'mason'
-home_dir	'/home/'+username+'/'
--- cookbooks/dev-essential/recipes/sublime-plugin.rb
+++ cookbooks/dev-essential/recipes/sublime-plugin.rb
@@ -4,7 +4,7 @@ username = Chef::Config[:username]
 #execute "mkdir -p #{installdir}"
 #execute "wget -P #{installdir} 'https://sublime.wbond.net/Package%20Control.sublime-package'"
 
-sublime_dir = Chef::Config[:home_dir] + '.config/sublime-text-2/Installed Packages/'
+sublime_dir = "#{ENV['HOME']}/.config/sublime-text-2/Installed Packages/"
 
 remote_file sublime_dir+'/Package Control.sublime-package' do
   source 'https://sublime.wbond.net/Package%20Control.sublime-package'
