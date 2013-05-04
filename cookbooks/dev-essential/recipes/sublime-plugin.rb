067f80e0a713624351bce0802cf8b6e998f23364
Mason Du <mdu@benbria.com>
2013-05-04 10:17:09 -0400

    create sublime directory package directory

--- cookbooks/dev-essential/recipes/sublime-plugin.rb
+++ cookbooks/dev-essential/recipes/sublime-plugin.rb
@@ -1,10 +1,9 @@
 username = Chef::Config[:username]
 # Install Sublime packages
-#installdir = '/home/' + username + '/.config/sublime-text-2/Installed Packages'
-#execute "mkdir -p #{installdir}"
 #execute "wget -P #{installdir} 'https://sublime.wbond.net/Package%20Control.sublime-package'"
 
 sublime_dir = "#{ENV['HOME']}/.config/sublime-text-2/Installed Packages/"
+execute "mkdir -p #{sublime_dir}"
 
 remote_file sublime_dir+'/Package Control.sublime-package' do
   source 'https://sublime.wbond.net/Package%20Control.sublime-package'
