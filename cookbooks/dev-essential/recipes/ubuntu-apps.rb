7d4888356280b467dee75c2a0cabe94486973119
Mason Du <masondu@gmail.com>
2013-05-05 13:14:04 -0400

    minor fix for ubuntu apps recipe

--- cookbooks/dev-essential/recipes/ubuntu-apps.rb
+++ cookbooks/dev-essential/recipes/ubuntu-apps.rb
@@ -1,10 +1,10 @@
+# Keepass
 execute 'sudo apt-add-repository ppa:jtaylor/keepass'
 package('keepass2')
+package('xdotool')
 
 # Dropbox
 execute 'sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E'
-execute 'sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu raring main'
+execute 'sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu raring main"'
 package('nautilus-dropbox')
-
-execute 'cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86" | tar xzf -'
-execute "~/.dropbox-dist/dropboxd"
+execute 'dropbox autostart y'
